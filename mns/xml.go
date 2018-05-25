5a168deba4d19accc27d44c30236160c8e46d650
kyai <hukuang@163.com>
2018-05-25 11:40:25 +0800

    commit

--- mns/xml.go
+++ mns/xml.go
@@ -13,12 +13,12 @@ type XmlError struct {
 // 创建队列
 type XmlCreateQueueReq struct {
 	XMLName                xml.Name `xml:"Queue"`
-	DelaySeconds           int      // 发送到该 Queue 的所有消息默认将以DelaySeconds参数指定的秒数延后可被消费，单位为秒。	0-604800秒（7天）范围内某个整数值，默认值为0
-	MaximumMessageSize     int      // 发送到该Queue的消息体的最大长度，单位为byte。	1024(1KB)-65536（64KB）范围内的某个整数值，默认值为65536（64KB）。
-	MessageRetentionPeriod int      // 消息在该 Queue 中最长的存活时间，从发送到该队列开始经过此参数指定的时间后，不论消息是否被取出过都将被删除，单位为秒。	60 (1分钟)-1296000 (15 天)范围内某个整数值，默认值345600 (4 天)
-	VisibilityTimeout      int      // 消息从该 Queue 中取出后从Active状态变成Inactive状态后的持续时间，单位为秒。	1-43200(12小时)范围内的某个值整数值，默认为30（秒）
-	PollingWaitSeconds     int      // 当 Queue 中没有消息时，针对该 Queue 的 ReceiveMessage 请求最长的等待时间，单位为秒。	0-30秒范围内的某个整数值，默认为0（秒）
-	LoggingEnabled         bool     // 是否开启日志管理功能，True表示启用，False表示停用	True/False，默认为False
+	DelaySeconds           int      `xml:"DelaySeconds,omitempty"`           // 发送到该 Queue 的所有消息默认将以DelaySeconds参数指定的秒数延后可被消费，单位为秒。	0-604800秒（7天）范围内某个整数值，默认值为0
+	MaximumMessageSize     int      `xml:"MaximumMessageSize,omitempty"`     // 发送到该Queue的消息体的最大长度，单位为byte。	1024(1KB)-65536（64KB）范围内的某个整数值，默认值为65536（64KB）。
+	MessageRetentionPeriod int      `xml:"MessageRetentionPeriod,omitempty"` // 消息在该 Queue 中最长的存活时间，从发送到该队列开始经过此参数指定的时间后，不论消息是否被取出过都将被删除，单位为秒。	60 (1分钟)-1296000 (15 天)范围内某个整数值，默认值345600 (4 天)
+	VisibilityTimeout      int      `xml:"VisibilityTimeout,omitempty"`      // 消息从该 Queue 中取出后从Active状态变成Inactive状态后的持续时间，单位为秒。	1-43200(12小时)范围内的某个值整数值，默认为30（秒）
+	PollingWaitSeconds     int      `xml:"PollingWaitSeconds,omitempty"`     // 当 Queue 中没有消息时，针对该 Queue 的 ReceiveMessage 请求最长的等待时间，单位为秒。	0-30秒范围内的某个整数值，默认为0（秒）
+	LoggingEnabled         bool     `xml:"LoggingEnabled,omitempty"`         // 是否开启日志管理功能，True表示启用，False表示停用	True/False，默认为False
 }
 
 type XmlSendMessageReq struct {
